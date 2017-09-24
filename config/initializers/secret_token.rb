e3360418e75f27a28e9c0746707f25eeea5d9f6d
Ryuichi Hajikano <longyi55h@gmail.com>
2017-09-24 05:17:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DogtrainerManagementSystem::Application.config.secret_key_base = '732e3e223ec8b8af3fe15d9c51b02727cb21a5f5ebde29ed271953494534be419bceb125eacb364f7494f4cca4c1501689e9be724773620a298634f5c2009657'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DogtrainerManagementSystem::Application.config.session_store :cookie_store, key: '_dogtrainer_management_system_session'
