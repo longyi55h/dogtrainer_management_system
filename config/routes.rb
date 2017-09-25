4734f18634cb059cdb58bbcfaca26af89315b319
Ryuichi Hajikano <longyi55h@gmail.com>
2017-09-25 00:50:40 +0000

    Install the Bootstrap 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 DogtrainerManagementSystem::Application.routes.draw do
+  get "static_pages/login"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
